7ccef1c7cb819201b4e32e7d0fc744681e1d8099
akshaydeshmukh2455 <akshdeshmukh123@gmail.com>
2023-06-13 12:43:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//document.getElementById(count).innerText = 5
+
+// let fistBatch=5
+
+// let count = fistBatch + secondBatch;
+// console.log(count)
+
+function increment()
+{
+    console.log("The button was clicked")
+}
